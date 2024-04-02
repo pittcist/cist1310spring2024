3f62d0af1ad3dfa8b5774a85efe96bd9f3e8922a
Pitt CIST <ykw@pitt.edu>
2024-04-01 21:08:29 -0400

    tested

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('login.htm')
+def home():
+   return render_template('login.htm')
 
 @app.route('/flask')
 def hello_flask():  
@@ -28,6 +28,45 @@ def login():
 @app.route('/success/<name>')
 def success(name):
     return 'Welcome {0}'.format(name)
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES ('{0}','{1}','{2}','{3}')".format(nm,addr,city,pin))
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sqlite3.connect("database.db")
+   con.row_factory = sqlite3.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
+
 
 if __name__ == '__main__':
     app.run()
